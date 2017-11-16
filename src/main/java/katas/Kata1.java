9b24aee32520f1fe47bfa5d8b1668c91121a0517
Fernanda Caroline Santos <fcsantos@fitec.org.br>
2017-11-16 17:20:06 -0200

    Creating Kata1 and Kata2

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -1,22 +1,24 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import model.Movie;
-import util.DataUtil;
-
 import java.util.List;
 import java.util.Map;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import model.Movie;
+import util.DataUtil;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
     DataSource: DataUtil.getMovies()
-    Output: List of ImmutableMap.of("id", "5", "title", "Bad Boys")
+    Output: List of
 */
 public class Kata1 {
-    public static List<Map> execute() {
-        List<Movie> movies = DataUtil.getMovies();
-
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
-    }
+	public static List<Map> execute() {
+		List<Movie> movies = DataUtil.getMovies();
+		// List<Map<String, Integer>> moviesMap = movies.stream().collect(Collectors.toMap("id", Movie::getId));
+		// return moviesMap;
+		return ImmutableList.of(ImmutableMap.of("id", "5", "title", "Bad Boys"));
+	}
 }
--- src/test/java/katas/Kata2Test.java
+++ src/test/java/katas/Kata2Test.java
@@ -1,15 +1,17 @@
 package katas;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-
+import com.google.common.collect.ImmutableList;
 
 public class Kata2Test {
 
-    @Test
-    public void testExecute() {
-        Assert.assertThat(Kata2.execute().size(), equalTo(4));
-    }
+	@Test
+	public void testExecute() {
+		Assert.assertThat(Kata2.execute().size(), equalTo(2));
+		Assert.assertEquals(Kata2.execute(), ImmutableList.of(654356453, 675465));
+	}
 }
